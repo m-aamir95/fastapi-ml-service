6fb17cb3ebae24d7f38fda4a28b45f145de9d926
Aamir <djzaamir@gmail.com>
2023-09-18 17:27:22 +0500

    Created Directory structure for the sentiment service

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict:
     return {"message" : "Hello World, Hello FastAPI"}
+
+@app.get("/health")
+async def health_check() -> dict:
+    return {"Status" : "API is up and running", 
+            "Model_Status" : "Configured and Loaded"}
