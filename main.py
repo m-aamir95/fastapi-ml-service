c687e3ac1d39f77f39c5e4eeba89fe8fd4f03385
Aamir <djzaamir@gmail.com>
2023-09-18 18:18:47 +0500

    Added the overall structure of the sentiment router

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 
+from services import sentiment_router
 
 app = FastAPI()
 
+app.include_router(sentiment_router.router)
 
-@app.get("/")
+@app.get("/", tags=["General"])
 async def root() -> dict:
     return {"message" : "Hello World, Hello FastAPI"}
 
-@app.get("/health")
+@app.get("/health", tags=["General"])
 async def health_check() -> dict:
     return {"Status" : "API is up and running", 
             "Model_Status" : "Configured and Loaded"}
