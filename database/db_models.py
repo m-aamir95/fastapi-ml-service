334d54fd3cf119d948e0c6d8a2bbb171372a8166
Aamir <djzaamir@gmail.com>
2023-09-21 11:05:14 +0500

    Added User and SentimentText sqlalchemy Models

--- database/db_models.py
+++ database/db_models.py
@@ -6,8 +6,26 @@ from .database_connection import Base
 
 class User(Base):
 
-    __tablename__ = "users"
+    __tablename__ = "User"
     
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
+    # Establish a bi-directional relationship with SentimentText
+    sentiment_texts = relationship("SentimentText", back_populates="writter_by")
+
+
+class SentimentText(Base):
+
+    __tablename__ = "SentimentText"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("User.id"))
+    text_id = Column(String)
+    text = Column(String)
+    sentiment_bag = Column(String)
+    reviewed = Column(Boolean, default=False)
+
+    # Establish a bi-directional relationship with User
+    written_by = relationship("User", back_populates="sentiment_texts")
