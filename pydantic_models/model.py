123b8f2afcf3957c24a4ce042be7ec6b6adc1337
Aamir <djzaamir@gmail.com>
2023-09-23 10:37:53 +0500

    Added Pydantic Models for User and SentimentText

--- pydantic_models/model.py
+++ pydantic_models/model.py
@@ -1,3 +1,18 @@
 from pydantic import BaseModel
+from typing import Optional
+
+class UserBase(BaseModel):
+
+    id : str
+    username : str
+    hashed_password : str
 
 
+class SentimentTextBase(BaseModel):
+
+    id : str
+    user_id : str
+    text_id : Optional[str]
+    text : str
+    sentiment_bag : str
+    reviewed : bool
