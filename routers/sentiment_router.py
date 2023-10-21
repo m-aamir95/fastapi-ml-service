6dc43718e227e7b91132888de07b22bf50e87c25
Aamir <djzaamir@gmail.com>
2023-10-21 11:36:29 +0500

    Addded dependency injection for the Sentiment_Service

--- routers/sentiment_router.py
+++ routers/sentiment_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from pydantic_models import data_models
 
@@ -14,10 +14,25 @@ router = APIRouter(
 )
 
 
+
+#region Dependency Injection for the Sentiment Analysis Service
+class GetSentimentService():
+
+    def __init__(self):
+
+        self.text_analysis_service : SentimentService = SentimentServiceHuggingFace()
+
+    def __call__(self):
+        
+        return self.text_analysis_service
+
+sentimentService_dependency = GetSentimentService()
+#endregion
+
 @router.post("/get_sentiment_score")
-async def get_sentiment_score(req : data_models.SentimentTextAnalysisWebRequest) -> dict:
+async def get_sentiment_score(req : data_models.SentimentTextAnalysisWebRequest,
+                              text_analysis_service = Depends(sentimentService_dependency)) -> dict:
     
-    text_analysis_service : SentimentService = SentimentServiceHuggingFace()
 
     return text_analysis_service.get_text_analysis(req.text)
     
