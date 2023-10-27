1d92d57935672259b10e09bf91c898ae4b6652ab
Aamir <djzaamir@gmail.com>
2023-10-27 10:22:47 +0500

    House Keeping Commit

--- services/sentiment_service.py
+++ services/sentiment_service.py
@@ -32,8 +32,15 @@ class SentimentServiceHuggingFace(SentimentService):
     def get_text_analysis(self, req : data_models.SentimentTextAnalysisWebRequest, user_service : UserService) -> dict:
 
         # Authenticate the user
-        if user_service.verify_user_login(req.username, req.hashed_password) is None:
+        authenticated_user = user_service.verify_user_login(req.username, req.hashed_password)
+        if authenticated_user is None:
             raise HTTPException(status_code=400, detail="Invalid username or password")
+        
+        print("------ Printing the authenticated user ---------")
+        print(authenticated_user)
+        for u in authenticated_user:
+            print(u)
+        print("------------------------------------------------")
 
 
         return {"model_resp" : self.sentiment_analysis_pipeline(req.text)}
