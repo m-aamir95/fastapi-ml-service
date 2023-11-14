7439ee8fdd6081a728304c462b03d84d6c2e94d6
Aamir <djzaamir@gmail.com>
2023-11-14 15:10:13 +0500

    HotFix: Python context management was not properly setup, Minor fix-4

--- database/database_connection.py
+++ database/database_connection.py
@@ -8,7 +8,7 @@ SQLALCHEMY_DATABASE_URL = "mysql://root:my-secret-pw@db/sentiment_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
 
 CustomizedDBSessionFactory =  sessionmaker(autocommit=False, autoflush=False, bind=engine)
-CustomizedDBSession = CustomizedDBSessionFactory
+CustomizedDBSession = CustomizedDBSessionFactory()
 
 
 Base = declarative_base()
